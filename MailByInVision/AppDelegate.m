ef5f11666ec873732bdbba70842f34105124557c
Michal Kalis <michal.kalis@gmail.com>
2016-03-16 23:48:17 +0100

    Add black background for status bar

--- MailByInVision/AppDelegate.m
+++ MailByInVision/AppDelegate.m
@@ -21,6 +21,8 @@
 
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
+    [self addBlackStatusBarView];
+    
     // Initialize Core Data stack
     [self setCoreDataStack:[[CoreDataStack alloc] initWithCallback:^{
         [self completeUserInterface];
@@ -41,7 +43,20 @@
     [[self coreDataStack] save];
 }
 
+#pragma mark - Auxiliary
+
 - (void)completeUserInterface {
+}
+
+- (void)addBlackStatusBarView {
+    UIView *view = [[UIView alloc] initWithFrame:CGRectZero];
+    view.translatesAutoresizingMaskIntoConstraints = NO;
+    view.backgroundColor = [UIColor blackColor];
+    [self.window.rootViewController.view addSubview:view];
+    
+    [self.window.rootViewController.view addConstraints:[NSLayoutConstraint constraintsWithVisualFormat:@"H:|[view]|" options:0 metrics:nil views:@{@"view": view}]];
+    [self.window.rootViewController.view addConstraints:[NSLayoutConstraint constraintsWithVisualFormat:@"V:|[view]" options:0 metrics:nil views:@{@"view": view}]];
+    [self.window.rootViewController.view addConstraint:[NSLayoutConstraint constraintWithItem:view attribute:NSLayoutAttributeHeight relatedBy:NSLayoutRelationEqual toItem:nil attribute:0 multiplier:1 constant:20]];
 }
 
 @end
