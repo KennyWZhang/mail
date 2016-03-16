208fb36a4a83795d74b1b8153c66a244b5349781
Michal Kalis <michal.kalis@gmail.com>
2016-03-16 23:45:37 +0100

    Create file structure

---
+++ MailByInVision/Controller/OtherViewController.h
@@ -0,0 +1,13 @@
+//
+//  OtherViewController.h
+//  MailByInVision
+//
+//  Created by Michal Kalis on 16/03/16.
+//  Copyright © 2016 Michal Kalis. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface OtherViewController : UIViewController
+
+@end
